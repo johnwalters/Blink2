6772997cc48fd53cf65453b1487d8cdb498ee265
John Walters <john.walters.92117@gmail.com>
2022-07-09 11:34:15 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+
+/*
+ * ssh admin@192.168.68.167
+ publish, then scp -r C:/projects/Blink2/bin/Release/net6.0/publish/* admin@192.168.68.167:~/projects/Blink2/
+ */
+
+Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+int pin = 27;
+using var controller = new GpioController();
+controller.OpenPin(pin, PinMode.Output);
+bool ledOn = true;
+while (true)
+{
+    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+    Thread.Sleep(2000);
+    ledOn = !ledOn;
+}
